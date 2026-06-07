dcc8dc67c3da5f37b777e553f46243dea3764c99
Stefano <schmidtsigma@gmail.com>
2026-06-07 16:17:14 +0300

    Implement From<FromUtf16Error> for Error

--- src/misc/error.rs
+++ src/misc/error.rs
@@ -1,4 +1,7 @@
+use std::string::FromUtf16Error;
+
 pub type Win32Error = windows::core::Error;
+pub const OK: Result<(), Error> = Ok(());
 
 #[derive(Debug)]
 pub enum Error {
@@ -6,12 +9,14 @@ pub enum Error {
 	Other(String)
 }
 
-pub fn ok() -> Result<(), Error> {
-	Ok(())
-}
-
 impl From<Win32Error> for Error {
 	fn from(value: Win32Error) -> Self {
 		Self::Win32(value)
+	}
+}
+
+impl From<FromUtf16Error> for Error {
+	fn from(value: FromUtf16Error) -> Self {
+		Self::Other(value.to_string())
 	}
 }
